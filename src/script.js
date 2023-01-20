4c52c69756cfec7af9f4dfed278461ba611afd48
Jude Bislig <judebislig@gmail.com>
2023-01-20 00:32:52 -0500

    changed images

--- src/script.js
+++ src/script.js
@@ -1,4 +1,17 @@
+// Toggle dark mode
+
 function toggleDark() {
     var element = document.body;
     element.classList.toggle("dark");
- }
+}
+
+
+// Scroll to Top Button
+
+function scrollUp() {
+    window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+};
